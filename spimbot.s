2ac9d4361bb56f6b2ccd80cbf4ae7c13c2a78ce4
Wenjie Liu <lwjieliu@gmail.com>
2020-11-25 22:54:55 -0600

    Random number generator strategy

--- spimbot.s
+++ spimbot.s
@@ -35,65 +35,8 @@ GRIDSIZE = 8
 has_puzzle:        .word 0                         
 puzzle:      .half 0:2000             
 heap:        .half 0:2000
+state:       .word 1 # Used to generate random number
 #### Puzzle
-
-rng:  # Random number generator using Xorshift technique
-        li $t0, 13
-        li $t1, 17
-        li $t2, 5
-        sll $t3, $a0, $t0
-        xor $a0, $a0, $t3
-
-        srl $t3, $a0, $t1
-        xor $a0, $a0, $t3
-
-        sll $t3, $a0, $t2
-        xor $a0, $a0, $t3
-
-        andi $t0, $a0, 0x111 # Get last 3 bits, value range 0-7
-        addi $t0, $t0, 1 # value range 1-8
-
-        li $t1, 45
-        mul  $v0, $t0, $t1 # return value range 45-360
-        jr $ra
-
-
-puzzle_solving:
-        sw $zero, has_puzzle
-        la $t0, puzzle
-        sw $t0, REQUEST_PUZZLE
-        while_loop:     
-                lw $t1, has_puzzle
-                bne $zero, $t1, end_while_loop
-                j while_loop
-        end_while_loop:
-        sub $sp, $sp, 24
-        sw $t0, 0($sp)
-        sw $t1, 4($sp)
-        sw $t2, 8($sp)
-        sw $t3, 12($sp)
-        sw $t4, 16($sp)
-        sw $a0, 20($sp)
-
-        la $a0, puzzle
-        la $a1, heap
-        li $a2, 0
-        li $a3, 0
-        
-        jal solve
-                
-        lw $t0, 0($sp)
-        lw $t1, 4($sp)
-        lw $t2, 8($sp)
-        lw $t3, 12($sp)
-        lw $t4, 16($sp)
-        lw $a0, 20($sp)
-        add $sp, $sp, 24
-        la $t4, heap
-        sw $t4, SUBMIT_SOLUTION
-        jr $ra
-
-
 
 .text
 main:
@@ -107,40 +50,291 @@ main:
 
 # Fill in your code here
 
-        sub $sp, $sp, 24
-        sw $t0, 0($sp)
-        sw $t1, 4($sp)
-        sw $t2, 8($sp)
-        sw $t3, 12($sp)
-        sw $t4, 16($sp)
-        sw $a0, 20($sp)
-        
-        jal puzzle_solving
-                
-        lw $t0, 0($sp)
-        lw $t1, 4($sp)
-        lw $t2, 8($sp)
-        lw $t3, 12($sp)
-        lw $t4, 16($sp)
-        lw $a0, 20($sp)
-        add $sp, $sp, 24
-
-
         lw $t0, TIMER
-        add $t0, $t0, 1000
+        add $t0, $t0, 10000
         sw $t0, TIMER
         li $t0, 10
         sw $t0, VELOCITY
 
-        li $t1, 90
+        li $t1, 45
         sw $t1, ANGLE
         li $t1, 1
         sw $t1, ANGLE_CONTROL
 infinite:
         j       infinite              # Don't remove this! If this is removed, then your code will not be graded!!!
 
+# The contents of this file are not graded, it exists purely as a reference solution that you can use
+
+
+encode_domino:
+        bge     $a0, $a1, encode_domino_greater_row
+
+        mul     $v0, $a0, $a2           # col * max_dots
+        add     $v0, $v0, $a1           # col * max_dots + row
+        add     $v0, $v0, 1             # col * max_dots + row + 1
+        j       encode_domino_end
+encode_domino_greater_row:
+        mul     $v0, $a1, $a2           # row * max_dots
+        add     $v0, $v0, $a0           # row * max_dots + col
+        add     $v0, $v0, 1             # col * max_dots + row + 1
+encode_domino_end:
+        jr      $ra
+
+next:
+        move    $v0, $a0
+        sub     $t0, $a2, 1
+        bne     $a1, $t0, next_col
+        add     $v0, $v0, 1
+next_col:
+        add     $t1, $a1, 1
+        rem     $v1, $t1, $a2
+
+        jr      $ra
+
+solve:
+        sub     $sp, $sp, 80
+        sw      $ra, 0($sp)
+        sw      $s0, 4($sp)
+        sw      $s1, 8($sp)
+        sw      $s2, 12($sp)
+        sw      $s3, 16($sp)
+        sw      $s4, 20($sp)
+        sw      $s5, 24($sp)
+        sw      $s6, 28($sp)
+        sw      $s7, 32($sp)
+        
+        move    $s0, $a0                # puzzle
+        move    $s1, $a1                # solution
+        move    $s2, $a2                # row
+        move    $s3, $a3                # col
+
+        lw      $s4, 0($s0)             # puzzle->num_rows
+        lw      $s5, 4($s0)             # puzzle->num_cols
+        lw      $s6, 8($s0)             # puzzle->max_dots
+        la      $s7, 268($s0)           # puzzle->dominos_used
+
+        mul     $t0, $s2, $s5
+        add     $t0, $t0, $s3           # row * num_cols + col
+        add     $t1, $s2, 1
+        mul     $t1, $t1, $s5
+        add     $t1, $t1, $s3           # (row + 1) * num_cols + col
+        mul     $t2, $s2, $s5
+        add     $t2, $t2, $s3
+        add     $t2, $t2, 1             # row * num_cols + (col + 1)
+
+        la      $t3, 12($s0)            # puzzle->board
+        add     $t4, $t3, $t0
+        lbu     $t9, 0($t4)
+        sw      $t9, 44($sp)            # puzzle->board[row * num_cols + col]
+        add     $t4, $t3, $t1
+        lbu     $t9, 0($t4)
+        sw      $t9, 48($sp)            # puzzle->board[(row + 1) * num_cols + col]
+        add     $t4, $t3, $t2
+        lbu     $t9, 0($t4)
+        sw      $t9, 52($sp)            # puzzle->board[row * num_cols + (col + 1)]
+
+        add     $t9, $s1, $t0
+        sw      $t9, 56($sp)            # &solution[row * num_cols + col]
+        add     $t9, $a1, $t1
+        sw      $t9, 60($sp)            # &solution[(row + 1) * num_cols + col]
+        add     $t9, $a1, $t2
+        sw      $t9, 64($sp)            # &solution[row * num_cols + (col + 1)]
+
+        move    $a0, $s2
+        move    $a1, $s3
+        move    $a2, $s5
+        jal     next
+        sw      $v0, 36($sp)
+        sw      $v1, 40($sp)
+
+
+        sge     $t0, $s2, $s4
+        sge     $t1, $s3, $s5
+        or      $t0, $t0, $t1
+        beq     $t0, 0, solve_not_base
+
+        li      $v0, 1
+        j       solve_end
+solve_not_base:
+
+
+        lw      $t0, 56($sp)
+        lb      $t0, 0($t0)
+        beq     $t0, 0, solve_not_solved
+
+        move    $a0, $s0
+        move    $a1, $s1
+        move    $a2, $v0
+        move    $a3, $v1
+        jal     solve
+        j       solve_end
+
+solve_not_solved:
+        lw      $t9, 44($sp)            # puzzle->board[row * num_cols + col]
+
+
+        sub     $t5, $s4, 1
+        bge     $s2, $t5, end_vert
+
+        lw      $t0, 60($sp)
+        lbu     $t8, 0($t0)             # solution[(row + 1) * num_cols + col]
+        bne     $t8, 0, end_vert 
+
+
+        move    $a0, $t9
+        lw      $a1, 48($sp)
+        move    $a2, $s6
+        jal     encode_domino
+        sw      $v0, 68($sp)
+
+        add     $t0, $s7, $v0
+        lbu     $t1, 0($t0)
+        bne     $t1, 0, end_vert
+
+        li      $t1, 1
+        sb      $t1, 0($t0)
+
+        lw      $t0, 56($sp)
+        sb      $v0, 0($t0)
+        lw      $t0, 60($sp)
+        sb      $v0, 0($t0)
+
+        move    $a0, $s0
+        move    $a1, $s1
+        lw      $a2, 36($sp)
+        lw      $a3, 40($sp)
+        jal     solve
+        beq     $v0, 0, end_vert_if
+        
+        li      $v0, 1
+        j       solve_end
+end_vert_if:
+
+        lw      $v0, 68($sp)            # domino_code
+        add     $t0, $v0, $s7
+        sb      $zero, 0($t0)
+
+        lw      $t0, 56($sp)
+        sb      $zero, 0($t0)
+
+        lw      $t0, 60($sp)
+        sb      $zero, 0($t0)
+
+
+end_vert:
+        sub     $t5, $s5, 1
+        bge     $s3, $t5, ret_0
+        lw      $t0, 64($sp)
+        lbu     $t1, 0($t0)             # solution[row * num_cols + (col + 1)]
+        bne     $t1, 0, ret_0
+
+        lw      $a0, 44($sp)            # puzzle->board[row * num_cols + col]
+        lw      $a1, 52($sp)
+        move    $a2, $s6
+        jal     encode_domino
+        sw      $v0, 68($sp)
+
+        add     $t0, $s7, $v0
+        lbu     $t1, 0($t0)
+        bne     $t1, 0, ret_0
+        
+        li      $t1, 1
+        sb      $t1, 0($t0)
+
+        lw      $t0, 56($sp)
+        sb      $v0, 0($t0)
+
+        lw      $t0, 64($sp)
+        sb      $v0, 0($t0)
+
+        move    $a0, $s0
+        move    $a1, $s1
+        lw      $a2, 36($sp)
+        lw      $a3, 40($sp)
+        jal     solve
+        beq     $v0, 0, end_horz_if
+        
+        li      $v0, 1
+        j       solve_end
+end_horz_if:
+
+        lw      $v0, 68($sp) # domino_code
+        add     $t0, $s7, $v0 
+        sb      $zero, 0($t0)
+        
+        lw      $t0, 56($sp)
+        sb      $zero, 0($t0)
+        lw      $t0, 64($sp)
+        sb      $zero, 0($t0)
+
+ret_0:
+        li      $v0, 0
+
+solve_end:
+        lw      $ra, 0($sp)
+        lw      $s0, 4($sp)
+        lw      $s1, 8($sp)
+        lw      $s2, 12($sp)
+        lw      $s3, 16($sp)
+        lw      $s4, 20($sp)
+        lw      $s5, 24($sp)
+        lw      $s6, 28($sp)
+        lw      $s7, 32($sp)
+        add     $sp, $sp, 80
+        jr      $ra
+
+zero:
+        li      $t0, 0          # i = 0
+zero_loop:
+        bge     $t0, $a0, zero_end_loop
+        add     $t1, $a1, $t0
+        sb      $zero, 0($t1)
+        add     $t0, $t0, 1
+        j       zero_loop
+zero_end_loop:
+        jr      $ra
+
+slow_solve_dominosa:
+        sub     $sp, $sp, 16
+        sw      $ra, 0($sp)
+        sw      $s0, 4($sp)
+        sw      $s1, 8($sp)
+        sw      $s2, 12($sp)
+
+        move    $s0, $a0
+        move    $s1, $a1
+
+        lw      $t0, 0($s0)
+        lw      $t1, 4($s0)
+        mul     $a0, $t0, $t1
+        jal     zero
+
+        li      $a0, 226
+        la      $a1, 268($s0)
+        jal     zero
+
+        move    $a0, $s0
+        move    $a1, $s1
+        li      $a2, 0
+        li      $a3, 0
+        jal     solve
+
+        lw      $ra, 0($sp)
+        lw      $s0, 4($sp)
+        lw      $s1, 8($sp)
+        lw      $s2, 12($sp)
+        add     $sp, $sp, 16
+
+        jr      $ra
+
+
+
+
+
+
+
 .kdata
-chunkIH:    .space 8  #TODO: Decrease this
+chunkIH:    .space 8  # TODO: Decrease this
 non_intrpt_str:    .asciiz "Non-interrupt exception\n"
 unhandled_str:    .asciiz "Unhandled interrupt type\n"
 .ktext 0x80000180
@@ -181,31 +375,31 @@ bonk_interrupt:
         li $t1, 10
         sw $t1, VELOCITY
 
-        sub $sp, $sp, 24
-        sw $ra, 0($sp)
-        sw $a0, 4($sp)
-        sw $t0, 8($sp)
-        sw $t1, 12($sp)
-        sw $t2, 16($sp)
-        sw $t3, 20($sp)
+        lw $t4, state
 
-        lw $a0, ANGLE
-        addi $a0, $a0, 1 # Initial input value to RNG cannot be zero
-        jal rng
+        li $t0, 13
+        li $t1, 17
+        li $t2, 5
+        sll $t3, $t4, $t0
+        xor $t4, $t4, $t3
 
-        lw $ra, 0($sp)
-        lw $a0, 4($sp)
-        lw $t0, 8($sp)
-        lw $t1, 12($sp)
-        lw $t2, 16($sp)
-        lw $t3, 20($sp)
-        add $sp, $sp, 24
-        li $t1, 0
-        sw $t1, ANGLE_CONTROL
-        sw $v0, ANGLE
+        srl $t3, $t4, $t1
+        xor $t4, $t4, $t3
 
+        sll $t3, $t4, $t2
+        xor $t4, $t4, $t3
 
+        sw $t4, state
+        li $t1, 360
+        rem $t5, $t4, $t1
 
+        sw $t5, ANGLE
+        li $t0, 1
+        sw $t0, ANGLE_CONTROL
+
+        lw $t0, TIMER
+        add $t0, $t0, 3000
+        sw $t0, TIMER
         j       interrupt_dispatch      # see if other interrupts are waiting
 
 request_puzzle_interrupt:
@@ -221,27 +415,32 @@ timer_interrupt:
         li $t0, 1
         sw $t0, PICKUP
 
-        lw $t0, TIMER
-        add $t0, $t0, 1000
-        sw $t0, TIMER
+        lw $t4, state
 
-        sub $sp, $sp, 24
-        sw $t0, 0($sp)
-        sw $t1, 4($sp)
-        sw $t2, 8($sp)
-        sw $t3, 12($sp)
-        sw $t4, 16($sp)
-        sw $a0, 20($sp)
-        
-        jal puzzle_solving
-                
-        lw $t0, 0($sp)
-        lw $t1, 4($sp)
-        lw $t2, 8($sp)
-        lw $t3, 12($sp)
-        lw $t4, 16($sp)
-        lw $a0, 20($sp)
-        add $sp, $sp, 24
+        li $t0, 13
+        li $t1, 17
+        li $t2, 5
+        sll $t3, $t4, $t0
+        xor $t4, $t4, $t3
+
+        srl $t3, $t4, $t1
+        xor $t4, $t4, $t3
+
+        sll $t3, $t4, $t2
+        xor $t4, $t4, $t3
+
+        sw $t4, state
+        li $t1, 360
+        rem $t5, $t4, $t1
+
+        sw $t5, ANGLE
+        li $t0, 1
+        sw $t0, ANGLE_CONTROL
+
+
+        lw $t0, TIMER
+        add $t0, $t0, 3000
+        sw $t0, TIMER
 
         j   interrupt_dispatch
 non_intrpt:                             # was some non-interrupt
