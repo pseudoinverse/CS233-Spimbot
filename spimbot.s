dee76e7bd2efa55918c8e91e49ff7bf6c2cfb356
lipingd2 <lipingd2@illinois.edu>
2020-11-28 23:24:22 -0600

    diagonal

--- spimbot.s
+++ spimbot.s
@@ -28,42 +28,266 @@ REQUEST_PUZZLE_ACK      = 0xffff00d8  ## Puzzle
 
 PICKUP                  = 0xffff00f4
 
-# Add any MMIO that you need here (see the Spimbot Documentation)
-
 ### Puzzle
 GRIDSIZE = 8
-has_puzzle:        .word 0                         
+
+has_puzzle:  .word 0                         
 puzzle:      .half 0:2000             
 heap:        .half 0:2000
-state:       .word 1 # Used to generate random number
 #### Puzzle
 
+
+
 .text
+
 main:
 # Construct interrupt mask
-	    li      $t4, 0
+            li      $t4, 0
         or      $t4, $t4, REQUEST_PUZZLE_INT_MASK # puzzle interrupt bit
-        or      $t4, $t4, TIMER_INT_MASK	  # timer interrupt bit
-        or      $t4, $t4, BONK_INT_MASK	  # timer interrupt bit
+        or      $t4, $t4, TIMER_INT_MASK          # timer interrupt bit
+        or      $t4, $t4, BONK_INT_MASK   # timer interrupt bit
         or      $t4, $t4, 1                       # global enable
-	    mtc0    $t4, $12
+            mtc0    $t4, $12
 
-# Fill in your code here
+#Fill in your code here
+la $v0, puzzle
+la $v1, REQUEST_PUZZLE
+sw $v0, ($v1)
 
-        lw $t0, TIMER
-        add $t0, $t0, 10000
-        sw $t0, TIMER
-        li $t0, 10
-        sw $t0, VELOCITY
+while:
+        la $s0, has_puzzle
+        lw $s0, 0($s0)
+        beq $s0,0, while
 
-        li $t1, 45
-        sw $t1, ANGLE
-        li $t1, 1
-        sw $t1, ANGLE_CONTROL
-infinite:
-        j       infinite              # Don't remove this! If this is removed, then your code will not be graded!!!
+la $a0, puzzle
+la $a1, heap
 
-# The contents of this file are not graded, it exists purely as a reference solution that you can use
+jal slow_solve_dominosa
+
+la $v1, SUBMIT_SOLUTION
+la $v0, heap
+sw $v0, ($v1)
+
+
+li $s1, 0
+
+li $s0, 0
+la $s2, BOT_X
+lw $s2, ($s2)
+li $s3, 10
+blt $s2, $s3, do_move
+li $s0, 7
+do_move:
+
+
+
+#Fill in your code here
+nimabi:
+
+beq $s0, 0, phase0
+beq $s0, 1, phase1
+beq $s0, 2, phase2
+beq $s0, 3, phase3
+beq $s0, 4, phase4
+beq $s0, 5, phase5
+beq $s0, 6, phase6
+beq $s0, 7, phase_stop
+
+phase0:
+        bne $s1, 20000, phase0_cont
+        li $s1, 0
+        li $s0, 1
+        b phase1
+        phase0_cont:
+
+        li $v0, 1
+        la $v1, ANGLE_CONTROL
+        sw $v0, ($v1)
+
+        li $v0, 1
+        la $v1, PICKUP
+        sw $v0, ($v1)
+
+        li $v0, 45
+        la $v1, ANGLE
+        sw $v0, ($v1)
+
+        li $v0, 100
+        la $v1, VELOCITY
+        sw $v0, ($v1)
+
+        b nimabi_end
+
+phase1: 
+        bne $s1, 200, phase1_cont
+        li $s1, 0
+        li $s0, 2
+        b phase2
+        phase1_cont:
+
+        li $v0, 1
+        la $v1, ANGLE_CONTROL
+        sw $v0, ($v1)
+
+        li $v0, 1
+        la $v1, PICKUP
+        sw $v0, ($v1)
+
+        li $v0, 135
+        la $v1, ANGLE
+        sw $v0, ($v1)
+
+        li $v0, 100
+        la $v1, VELOCITY
+        sw $v0, ($v1)
+
+        b nimabi_end
+
+phase2: 
+        bne $s1, 20000, phase2_cont
+        li $s1, 0
+        li $s0, 3
+        b phase3
+        phase2_cont:
+
+        li $v0, 1
+        la $v1, ANGLE_CONTROL
+        sw $v0, ($v1)
+
+        li $v0, 1
+        la $v1, PICKUP
+        sw $v0, ($v1)
+
+        li $v0, 225
+        la $v1, ANGLE
+        sw $v0, ($v1)
+
+        li $v0, 100
+        la $v1, VELOCITY
+        sw $v0, ($v1)
+
+        b nimabi_end
+
+phase3: 
+        bne $s1, 200, phase3_cont
+        li $s1, 0
+        li $s0, 0
+        b phase4
+        phase3_cont:
+
+        li $v0, 1
+        la $v1, ANGLE_CONTROL
+        sw $v0, ($v1)
+
+        li $v0, 1
+        la $v1, PICKUP
+        sw $v0, ($v1)
+
+        li $v0, -35
+        la $v1, ANGLE
+        sw $v0, ($v1)
+
+        li $v0, 100
+        la $v1, VELOCITY
+        sw $v0, ($v1)
+
+        b nimabi_end
+
+phase4: 
+        bne $s1, 2700, phase4_cont
+        li $s1, 0
+        li $s0, 5
+        b phase5
+        phase4_cont:
+
+        li $v0, 1
+        la $v1, ANGLE_CONTROL
+        sw $v0, ($v1)
+
+        li $v0, 1
+        la $v1, PICKUP
+        sw $v0, ($v1)
+
+        li $v0, -35
+        la $v1, ANGLE
+        sw $v0, ($v1)
+
+        li $v0, 100
+        la $v1, VELOCITY
+        sw $v0, ($v1)
+
+        b nimabi_end
+
+phase5: 
+        bne $s1, 5000, phase5_cont
+        li $s1, 0
+        li $s0, 6
+        b phase6
+        phase5_cont:
+
+        li $v0, 1
+        la $v1, ANGLE_CONTROL
+        sw $v0, ($v1)
+
+        li $v0, 1
+        la $v1, PICKUP
+        sw $v0, ($v1)
+
+        li $v0, 36
+        la $v1, ANGLE
+        sw $v0, ($v1)
+
+        li $v0, 100
+        la $v1, VELOCITY
+        sw $v0, ($v1)
+
+        b nimabi_end
+
+phase6: 
+        bne $s1, 5000, phase6_cont
+        li $s1, 0
+        li $s0, 7
+        b phase_stop
+        phase6_cont:
+
+        li $v0, 1
+        la $v1, ANGLE_CONTROL
+        sw $v0, ($v1)
+
+        li $v0, 1
+        la $v1, PICKUP
+        sw $v0, ($v1)
+
+        li $v0, -123
+        la $v1, ANGLE
+        sw $v0, ($v1)
+
+        li $v0, 100
+        la $v1, VELOCITY
+        sw $v0, ($v1)
+
+        b nimabi_end
+
+phase_stop:
+
+        li $v0, 0
+        la $v1, PICKUP
+        sw $v0, ($v1)
+
+        li $v0, 0
+        la $v1, VELOCITY
+        sw $v0, ($v1)
+
+        b nimabi_end
+
+nimabi_end:
+
+addi $s1, $s1, 1
+b nimabi
+
+
+while1:
+        b while1
 
 
 encode_domino:
@@ -80,17 +304,90 @@ encode_domino_greater_row:
 encode_domino_end:
         jr      $ra
 
+# -------------------------------------------------------------------------
 next:
+        # $a0 = row
+        # $a1 = col
+        # $a2 = num_cols
+        # $v0 = next_row
+        # $v1 = next_col
+
+        #     int next_row = ((col == num_cols - 1) ? row + 1 : row);
         move    $v0, $a0
         sub     $t0, $a2, 1
         bne     $a1, $t0, next_col
         add     $v0, $v0, 1
 next_col:
+        #     int next_col = (col + 1) % num_cols;
         add     $t1, $a1, 1
         rem     $v1, $t1, $a2
 
         jr      $ra
 
+
+
+
+# // main solve function, recurse using backtrack
+# // puzzle is the puzzle question struct
+# // solution is an array that the function will fill the answer in
+# // row, col are the current location
+# // dominos_used is a helper array of booleans (represented by a char)
+# //   that shows which dominos have been used at this stage of the search
+# //   use encode_domino() for indexing
+# int solve(dominosa_question* puzzle, 
+#           unsigned char* solution,
+#           int row,
+#           int col) {
+#
+#     int num_rows = puzzle->num_rows;
+#     int num_cols = puzzle->num_cols;
+#     int max_dots = puzzle->max_dots;
+#     int next_row = ((col == num_cols - 1) ? row + 1 : row);
+#     int next_col = (col + 1) % num_cols;
+#     unsigned char* dominos_used = puzzle->dominos_used;
+#
+#     if (row >= num_rows || col >= num_cols) { return 1; }
+#     if (solution[row * num_cols + col] != 0) { 
+#         return solve(puzzle, solution, next_row, next_col); 
+#     }
+#
+#     unsigned char curr_dots = puzzle->board[row * num_cols + col];
+#
+#     if (row < num_rows - 1 && solution[(row + 1) * num_cols + col] == 0) {
+#         int domino_code = encode_domino(curr_dots,
+#                                         puzzle->board[(row + 1) * num_cols + col],
+#                                         max_dots);
+#
+#         if (dominos_used[domino_code] == 0) {
+#             dominos_used[domino_code] = 1;
+#             solution[row * num_cols + col] = domino_code;
+#             solution[(row + 1) * num_cols + col] = domino_code;
+#             if (solve(puzzle, solution, next_row, next_col)) {
+#                 return 1;
+#             }
+#             dominos_used[domino_code] = 0;
+#             solution[row * num_cols + col] = 0;
+#             solution[(row + 1) * num_cols + col] = 0;
+#         }
+#     }
+#     if (col < num_cols - 1 && solution[row * num_cols + (col + 1)] == 0) {
+#         int domino_code = encode_domino(curr_dots,
+#                                         puzzle->board[row * num_cols + (col + 1)],
+#                                         max_dots);
+#         if (dominos_used[domino_code] == 0) {
+#             dominos_used[domino_code] = 1;
+#             solution[row * num_cols + col] = domino_code;
+#             solution[row * num_cols + (col + 1)] = domino_code;
+#             if (solve(puzzle, solution, next_row, next_col)) {
+#                 return 1;
+#             }
+#             dominos_used[domino_code] = 0;
+#             solution[row * num_cols + col] = 0;
+#             solution[row * num_cols + (col + 1)] = 0;
+#         }
+#     }
+#     return 0;
+# }
 solve:
         sub     $sp, $sp, 80
         sw      $ra, 0($sp)
@@ -108,11 +405,18 @@ solve:
         move    $s2, $a2                # row
         move    $s3, $a3                # col
 
+#     int num_rows = puzzle->num_rows;
+#     int num_cols = puzzle->num_cols;
+#     int max_dots = puzzle->max_dots;
+#     unsigned char* dominos_used = puzzle->dominos_used;
         lw      $s4, 0($s0)             # puzzle->num_rows
         lw      $s5, 4($s0)             # puzzle->num_cols
         lw      $s6, 8($s0)             # puzzle->max_dots
         la      $s7, 268($s0)           # puzzle->dominos_used
 
+# Compute:
+# - next_row (Done below)
+# - next_col (Done below)
         mul     $t0, $s2, $s5
         add     $t0, $t0, $s3           # row * num_cols + col
         add     $t1, $s2, 1
@@ -133,6 +437,7 @@ solve:
         lbu     $t9, 0($t4)
         sw      $t9, 52($sp)            # puzzle->board[row * num_cols + (col + 1)]
 
+        # solution addresses
         add     $t9, $s1, $t0
         sw      $t9, 56($sp)            # &solution[row * num_cols + col]
         add     $t9, $a1, $t1
@@ -140,6 +445,9 @@ solve:
         add     $t9, $a1, $t2
         sw      $t9, 64($sp)            # &solution[row * num_cols + (col + 1)]
 
+
+        #     int next_row = ((col == num_cols - 1) ? row + 1 : row);
+        #     int next_col = (col + 1) % num_cols;
         move    $a0, $s2
         move    $a1, $s3
         move    $a2, $s5
@@ -148,6 +456,7 @@ solve:
         sw      $v1, 40($sp)
 
 
+#     if (row >= num_rows || col >= num_cols) { return 1; }
         sge     $t0, $s2, $s4
         sge     $t1, $s3, $s5
         or      $t0, $t0, $t1
@@ -157,7 +466,9 @@ solve:
         j       solve_end
 solve_not_base:
 
-
+#     if (solution[row * num_cols + col] != 0) { 
+#         return solve(puzzle, solution, next_row, next_col); 
+#     }
         lw      $t0, 56($sp)
         lb      $t0, 0($t0)
         beq     $t0, 0, solve_not_solved
@@ -170,9 +481,10 @@ solve_not_base:
         j       solve_end
 
 solve_not_solved:
+#     unsigned char curr_dots = puzzle->board[row * num_cols + col];
         lw      $t9, 44($sp)            # puzzle->board[row * num_cols + col]
 
-
+#     if (row < num_rows - 1 && solution[(row + 1) * num_cols + col] == 0) {
         sub     $t5, $s4, 1
         bge     $s2, $t5, end_vert
 
@@ -180,25 +492,35 @@ solve_not_solved:
         lbu     $t8, 0($t0)             # solution[(row + 1) * num_cols + col]
         bne     $t8, 0, end_vert 
 
-
+#         int domino_code = encode_domino(curr_dots,
+#                                         puzzle->board[(row + 1) * num_cols + col],
+#                                         max_dots);
         move    $a0, $t9
         lw      $a1, 48($sp)
         move    $a2, $s6
         jal     encode_domino
         sw      $v0, 68($sp)
 
+#         if (dominos_used[domino_code] == 0) {
         add     $t0, $s7, $v0
         lbu     $t1, 0($t0)
         bne     $t1, 0, end_vert
 
+#             dominos_used[domino_code] = 1;
         li      $t1, 1
         sb      $t1, 0($t0)
 
+#             solution[row * num_cols + col] = domino_code;
+#             solution[(row + 1) * num_cols + col] = domino_code;
         lw      $t0, 56($sp)
         sb      $v0, 0($t0)
         lw      $t0, 60($sp)
         sb      $v0, 0($t0)
 
+        
+#             if (solve(puzzle, solution, next_row, next_col)) {
+#                 return 1;
+#             }
         move    $a0, $s0
         move    $a1, $s1
         lw      $a2, 36($sp)
@@ -210,43 +532,56 @@ solve_not_solved:
         j       solve_end
 end_vert_if:
 
+#             dominos_used[domino_code] = 0;
         lw      $v0, 68($sp)            # domino_code
         add     $t0, $v0, $s7
         sb      $zero, 0($t0)
-
+        
+#             solution[row * num_cols + col] = 0;
         lw      $t0, 56($sp)
         sb      $zero, 0($t0)
-
+#             solution[(row + 1) * num_cols + col] = 0;
         lw      $t0, 60($sp)
         sb      $zero, 0($t0)
-
+#         }
+#     }
 
 end_vert:
+
+#     if (col < num_cols - 1 && solution[row * num_cols + (col + 1)] == 0) {
         sub     $t5, $s5, 1
         bge     $s3, $t5, ret_0
         lw      $t0, 64($sp)
         lbu     $t1, 0($t0)             # solution[row * num_cols + (col + 1)]
         bne     $t1, 0, ret_0
 
+#         int domino_code = encode_domino(curr_dots,
+#                                         puzzle->board[row * num_cols + (col + 1)],
+#                                         max_dots);
         lw      $a0, 44($sp)            # puzzle->board[row * num_cols + col]
         lw      $a1, 52($sp)
         move    $a2, $s6
         jal     encode_domino
         sw      $v0, 68($sp)
 
+#         if (dominos_used[domino_code] == 0) {
         add     $t0, $s7, $v0
         lbu     $t1, 0($t0)
         bne     $t1, 0, ret_0
         
+#             dominos_used[domino_code] = 1;
         li      $t1, 1
         sb      $t1, 0($t0)
 
+#             solution[row * num_cols + col] = domino_code;
         lw      $t0, 56($sp)
         sb      $v0, 0($t0)
-
+#             solution[row * num_cols + (col + 1)] = domino_code;
         lw      $t0, 64($sp)
         sb      $v0, 0($t0)
-
+#             if (solve(puzzle, solution, next_row, next_col)) {
+#                 return 1;
+#             }
         move    $a0, $s0
         move    $a1, $s1
         lw      $a2, 36($sp)
@@ -258,15 +593,23 @@ end_vert:
         j       solve_end
 end_horz_if:
 
+
+
+#             dominos_used[domino_code] = 0;
         lw      $v0, 68($sp) # domino_code
         add     $t0, $s7, $v0 
         sb      $zero, 0($t0)
         
+#             solution[row * num_cols + col] = 0;
         lw      $t0, 56($sp)
         sb      $zero, 0($t0)
+#             solution[row * num_cols + (col + 1)] = 0;
         lw      $t0, 64($sp)
         sb      $zero, 0($t0)
-
+#         }
+#     }
+#     return 0;
+# }
 ret_0:
         li      $v0, 0
 
@@ -283,6 +626,14 @@ solve_end:
         add     $sp, $sp, 80
         jr      $ra
 
+
+
+# // zero out an array with given number of elements
+# void zero(int num_elements, unsigned char* array) {
+#     for (int i = 0; i < num_elements; i++) {
+#         array[i] = 0;
+#     }
+# }
 zero:
         li      $t0, 0          # i = 0
 zero_loop:
@@ -294,6 +645,19 @@ zero_loop:
 zero_end_loop:
         jr      $ra
 
+
+
+
+# // the slow solve entry function,
+# // solution will appear in solution array
+# // return value shows if the dominosa is solved or not
+# int slow_solve_dominosa(dominosa_question* puzzle, unsigned char* solution) {
+#     zero(puzzle->num_rows * puzzle->num_cols, solution);
+#     zero(MAX_MAXDOTS * MAX_MAXDOTS, dominos_used);
+#     return solve(puzzle, solution, 0, 0);
+# }
+# // end of solution
+# /*** end of the solution to the puzzle ***/
 slow_solve_dominosa:
         sub     $sp, $sp, 16
         sw      $ra, 0($sp)
@@ -304,15 +668,20 @@ slow_solve_dominosa:
         move    $s0, $a0
         move    $s1, $a1
 
+
+
+#     zero(puzzle->num_rows * puzzle->num_cols, solution);
         lw      $t0, 0($s0)
         lw      $t1, 4($s0)
         mul     $a0, $t0, $t1
         jal     zero
 
+#     zero(MAX_MAXDOTS * MAX_MAXDOTS + 1, dominos_used);
         li      $a0, 226
         la      $a1, 268($s0)
         jal     zero
 
+#     return solve(puzzle, solution, 0, 0);
         move    $a0, $s0
         move    $a1, $s1
         li      $a2, 0
@@ -326,15 +695,11 @@ slow_solve_dominosa:
         add     $sp, $sp, 16
 
         jr      $ra
-
-
-
-
-
-
+infinite:
+        j       infinite              # Don't remove this! If this is removed, then your code will not be graded!!!
 
 .kdata
-chunkIH:    .space 8  # TODO: Decrease this
+chunkIH:    .space 8  #TODO: Decrease this
 non_intrpt_str:    .asciiz "Non-interrupt exception\n"
 unhandled_str:    .asciiz "Unhandled interrupt type\n"
 .ktext 0x80000180
@@ -343,6 +708,7 @@ interrupt_handler:
         move      $k1, $at              # Save $at
 .set at
         la      $k0, chunkIH
+
         sw      $a0, 0($k0)             # Get some free registers
         sw      $v0, 4($k0)             # by storing them to a global variable
 
@@ -361,8 +727,8 @@ interrupt_dispatch:                     # Interrupt:
         and     $a0, $k0, TIMER_INT_MASK # is there a timer interrupt?
         bne     $a0, 0, timer_interrupt
 
-        and 	$a0, $k0, REQUEST_PUZZLE_INT_MASK
-        bne 	$a0, 0, request_puzzle_interrupt
+        and     $a0, $k0, REQUEST_PUZZLE_INT_MASK
+        bne     $a0, 0, request_puzzle_interrupt
 
         li      $v0, PRINT_STRING       # Unhandled interrupt types
         la      $a0, unhandled_str
@@ -371,77 +737,21 @@ interrupt_dispatch:                     # Interrupt:
 
 bonk_interrupt:
         sw      $0, BONK_ACK
-# Fill in your code here
-        li $t1, 10
-        sw $t1, VELOCITY
-
-        lw $t4, state
-
-        li $t0, 13
-        li $t1, 17
-        li $t2, 5
-        sll $t3, $t4, $t0
-        xor $t4, $t4, $t3
-
-        srl $t3, $t4, $t1
-        xor $t4, $t4, $t3
-
-        sll $t3, $t4, $t2
-        xor $t4, $t4, $t3
-
-        sw $t4, state
-        li $t1, 360
-        rem $t5, $t4, $t1
-
-        sw $t5, ANGLE
-        li $t0, 1
-        sw $t0, ANGLE_CONTROL
-
-        lw $t0, TIMER
-        add $t0, $t0, 3000
-        sw $t0, TIMER
+#Fill in your code here
         j       interrupt_dispatch      # see if other interrupts are waiting
 
 request_puzzle_interrupt:
         sw      $0, REQUEST_PUZZLE_ACK
-# Fill in your code here
-        li $t0, 1       
-        sw $t0, has_puzzle
-        j	interrupt_dispatch
+#Fill in your code here
+        la $a0, has_puzzle
+        li $v0, 1
+        sw $v0, ($a0)
+
+        j       interrupt_dispatch
 
 timer_interrupt:
         sw      $0, TIMER_ACK
-# Fill in your code here
-        li $t0, 1
-        sw $t0, PICKUP
-
-        lw $t4, state
-
-        li $t0, 13
-        li $t1, 17
-        li $t2, 5
-        sll $t3, $t4, $t0
-        xor $t4, $t4, $t3
-
-        srl $t3, $t4, $t1
-        xor $t4, $t4, $t3
-
-        sll $t3, $t4, $t2
-        xor $t4, $t4, $t3
-
-        sw $t4, state
-        li $t1, 360
-        rem $t5, $t4, $t1
-
-        sw $t5, ANGLE
-        li $t0, 1
-        sw $t0, ANGLE_CONTROL
-
-
-        lw $t0, TIMER
-        add $t0, $t0, 3000
-        sw $t0, TIMER
-
+#Fill in your code here
         j   interrupt_dispatch
 non_intrpt:                             # was some non-interrupt
         li      $v0, PRINT_STRING
